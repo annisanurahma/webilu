3d7374d40ef60801b5f93c493f529dba15e523f0
annisanurahma <annisanurahmah59@gmail.com>
2025-08-11 06:10:40 +0700

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,24 +1,76 @@
-import Logo from '../img/logos/web-logo.png'
+import React, { useState } from "react";
+import Logo from '../img/logos/web-logo.png';
 
 const Navbar = () => {
-    return (
-        <>
-            <nav class="navbar navbar-light navbar-expand-md sticky-top navbar-shrink py-3 background-nav" id="mainNav">
-                <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><img class="img-logo" src={Logo} herf="home" /></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navcol-1">
-                        <ul class="navbar-nav mx-auto text-navbar">
-                            <li class="nav-item"><a class="nav-link active" href="home">BERANDA</a></li>
-                            <li class="nav-item"><a class="nav-link" href="panduan" >PANDUAN</a></li>
-                            <li class="nav-item"><a class="nav-link" href="data" >DATA</a></li>
-                            <li class="nav-item" ><a class="nav-link" href="kadidat" >KANDIDAT</a></li>
-                            <li class="nav-item" ><a class="nav-link" href="aboutus" >ABOUT US</a></li>
-                            <li class="nav-item"></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
-}
+  const [activeMenu, setActiveMenu] = useState("home"); // default home aktif
 
-export default Navbar
+  // fungsi handle klik untuk set menu aktif
+  const handleClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  return (
+    <nav className="navbar navbar-light navbar-expand-md sticky-top navbar-shrink py-3 background-nav" id="mainNav">
+      <div className="container">
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <img className="img-logo" src={Logo} alt="Logo" />
+        </a>
+        <button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1">
+          <span className="visually-hidden">Toggle navigation</span>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navcol-1">
+          <ul className="navbar-nav mx-auto text-navbar">
+            <li className="nav-item">
+              <a 
+                className={"nav-link " + (activeMenu === "home" ? "fw-bold" : "")} 
+                href="home" 
+                onClick={() => handleClick("home")}
+              >
+                BERANDA
+              </a>
+            </li>
+            <li className="nav-item">
+              <a 
+                className={"nav-link " + (activeMenu === "panduan" ? "fw-bold" : "")} 
+                href="panduan" 
+                onClick={() => handleClick("panduan")}
+              >
+                PANDUAN
+              </a>
+            </li>
+            <li className="nav-item">
+              <a 
+                className={"nav-link " + (activeMenu === "data" ? "fw-bold" : "")} 
+                href="data" 
+                onClick={() => handleClick("data")}
+              >
+                DATA
+              </a>
+            </li>
+            <li className="nav-item">
+              <a 
+                className={"nav-link " + (activeMenu === "kadidat" ? "fw-bold" : "")} 
+                href="kadidat" 
+                onClick={() => handleClick("kadidat")}
+              >
+                KANDIDAT
+              </a>
+            </li>
+            <li className="nav-item">
+              <a 
+                className={"nav-link " + (activeMenu === "aboutus" ? "fw-bold" : "")} 
+                href="aboutus" 
+                onClick={() => handleClick("aboutus")}
+              >
+                ABOUT US
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
